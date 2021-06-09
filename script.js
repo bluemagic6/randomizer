68a88d57ce3fac5d1a14aa208ee69b54422a9c27
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-06-08 22:02:03 -0500

    updated script fixed if statements

--- script.js
+++ script.js
@@ -1,129 +1,178 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate')
 
-var capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var capitalsArray =capitals.split("");
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var lowerArray = lower.split("");
-var special = "*&^%$#@!/?"
-var specialArray = special.split("");
-var number = "1234567890";
-var numberArray = number.split("");
-var Inputs
-var fullPassword= [];
+var capitalsArray = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+var lowerArray = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
 
-    passwordText.value = password;
+var specialArray = [
+    '@',
+    '%',
+    '+',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ];
+
+var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var fullPassword = []
+
+function getPrompts() {
+  var PWlength = parseInt(
+    prompt('How many characters do you want to use? (Must be between 8 and 128)')
+  );
+
+  if (PWlength === null || PWlength < 8 || PWlength > 128 || isNaN(PWlength)) {
+    alert('Invalid! Enter a number between 8 and 128');
+    return
+  }
+
+  var capitals = confirm('Would you like your password to contain UPPERCASE letters?',
+  )
+
+  var lower = confirm('Would you like your password to contain LOWERCASE letters?',
+  )
+
+  var special = confirm('Would you like your password to contain SPECIAL CHARACTERS?',
+  )
+
+  var number = confirm('Would you like your password to contain NUMBERS?')
+
+  var pwChoices = {
+    length: PWlength,
+    capitals: capitals,
+    lower: lower,
+    special: special,
+    number: number,
+  }
+
+  return pwChoices
 }
 
 function generatePassword() {
-    var PWlength = prompt("How many characters do you want to use? (Must be between 8 and 128)")
-    if (PWlength == null || PWlength<8 || PWlength>128 || isNaN(PWlength)) {
-        alert("Invalid! Enter a number between 8 and 128");
-        return}
-        console.log("password length " + PWlength)
-    var capitals = confirm("Would you like your password to contain UPPERCASE letters?");
-        console.log("capitals " + capitals)
-    var lower = confirm("Would you like your password to contain LOWERCASE letters?");
-        console.log("lower case " + lower)
-    var special = confirm("Would you like your password to contain SPECIAL CHARACTERS?");
-        console.log("Special Characters " + special)
-    var number = confirm("Would you like your password to contain NUMBERS?");
-        console.log("Numbers " + number)
+  var choices = getPrompts()
 
-if(!capitals && !lower && !special && !number) {
-    Inputs= alert("No items selected, Please your password characters")
-    console.log(Inputs);
-}
-// true CAPITAL LOWER NUMBER SPECIAL
-else if(capitals && number && lower && special){
-    Inputs= capitalsArray.concat(lowerArray, numberArray, specialArray);
-    console.log(Inputs);
-}
-// True CAPITAL LOWER NUMBER
-else if(capitals && number && lower){
-    Inputs= capitalsArray.concat(numberArray, lowerArray);
-    console.log(Inputs);
-}
-// True LOWER NUMBER SPECIAL
-else if(number && lower && special){
-    Inputs= numberArray.concat(lowerArray, specialArray);
-    console.log(Inputs);
-}
-// true CAPITAL LOWER SPECIAL
-else if(capitals && lower && special){
-    Inputs= capitalsArray.concat(lowerArray, specialArray);
-    console.log(Inputs);
-}
-// true CAPITAL NUMBER SPECIAL
-else if(capitals && number && special){
-    Inputs= capitalsArray.concat(numberArray, specialArray);
-    console.log(Inputs);
-}
-// True LOWER NUMBER
-else if(number && lower ){
-    Inputs= numberArray.concat(lowerArray);
-    console.log(Inputs);
-}
-// True CAPTIAL NUMBER
-else if(capitals && number){
-    Inputs= capitalsArray.concat(numberArray);
-    console.log(Inputs);
-}
-// True LOWER SPECIAL
-else if(lower && special){
-    Inputs= lowerArray.concat(specialArray);
-    console.log(Inputs);
-}
-// True SPECIAL CAPITAL
-else if(capitals && special){
-    Inputs= capitalsArray.concat(specialArray);
-    console.log(Inputs);
-}
-// True NUMBER SPECIAL
-else if(number && special){
-    Inputs= numberArray.concat(specialArray);
-    console.log(Inputs);
-}
-// True CAPITAL LOWER
-else if(capitals && lower ){
-    Inputs= capitalsArray.concat(lowerArray);
-    console.log(Inputs);
-}
-// True CAPITAL
-else if(capitals){
-    Inputs= capitalsArray;
-    console.log(Inputs);
-}
-// True LOWER
-else if(lower){
-    Inputs= lowerArray;
-    console.log(Inputs);
-}
-// True NUMBER
-else if(number){
-    Inputs= numberArray;
-    console.log(Inputs);
-}
-// True SPECIAL
-else if(special){
-    Inputs= specialsArray;
-    console.log(Inputs);
+  var possArr = []
+
+  var chosenChar = []
+
+  if (choices.capitals) {
+    possArr = possArr.concat(capitalsArray)
+    chosenChar.push(makeRandom(capitalsArray))
+  }
+
+  if (choices.lower) {
+    possArr = possArr.concat(lowerArray)
+    chosenChar.push(makeRandom(lowerArray))
+  }
+
+  if (choices.number) {
+    possArr = possArr.concat(numberArray)
+    chosenChar.push(makeRandom(numberArray))
+  }
+
+  if (choices.special) {
+    possArr = possArr.concat(specialArray)
+    chosenChar.push(makeRandom(specialArray))
+  }
+
+  for (var i = 0; i < choices.length; i++) {
+    var newCharacter = makeRandom(possArr)
+    fullPassword.push(newCharacter)
+  }
+
+  for (var i = 0; i < chosenChar.length; i++) {
+    fullPassword[i] = chosenChar[i]
+  }
+
+  var password = fullPassword.join('')
+  console.log('Your Password is: ' + fullPassword)
+  return password
 }
 
-for (var i = 0; i < PWlength; i++) {
-    var newCharacter = Inputs[Math.floor(Math.random() * Inputs.length)];
-    fullPassword.push(newCharacter);
-    console.log(newCharacter);
+function makeRandom(arr) {
+  var ranInd = Math.floor(Math.random() * arr.length)
+  var ranEl = arr[ranInd]
+  return ranEl
 }
 
-var password = fullPassword.join("");
-    console.log("Your Password is: " + fullPassword);
-    return password;
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword()
+  var passwordText = document.querySelector('#password')
+
+  passwordText.value = password
 }
+console.log(generatePassword)
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword)
